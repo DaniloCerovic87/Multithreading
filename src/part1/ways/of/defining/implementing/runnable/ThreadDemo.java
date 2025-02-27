29b8e2610efd31a2d95bb5c83074e7e6e6418cc6
DaniloCerovic87 <chessteacher87@gmail.com>
2025-02-27 18:17:16 +0100

    Defining a thread by implementing runnable

---
+++ src/part1/ways/of/defining/implementing/runnable/ThreadDemo.java
@@ -0,0 +1,14 @@
+package part1.ways.of.defining.implementing.runnable;
+
+public class ThreadDemo {
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("main Thread");
+        }
+    }
+
+}
