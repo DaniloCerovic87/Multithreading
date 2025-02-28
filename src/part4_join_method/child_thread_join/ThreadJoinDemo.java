6e3ebf73fc660529bf72e330daac6107da976a3b
DaniloCerovic87 <chessteacher87@gmail.com>
2025-02-28 20:18:30 +0100

    Thread join - added possible forms of a deadlock under comments

--- src/part4_join_method/child_thread_join/ThreadJoinDemo.java
+++ src/part4_join_method/child_thread_join/ThreadJoinDemo.java
@@ -5,8 +5,12 @@ public class ThreadJoinDemo {
     public static void main(String[] args) throws InterruptedException {
         MyThread.mainThread = Thread.currentThread();
 
+//        Thread.currentThread().join(); causes form of a deadlock
+
         MyThread t = new MyThread();
         t.start();
+
+//        t.join(); causes form of a deadlock
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Main Thread");
