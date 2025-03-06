bbc7dc403ba4a7232d65a2a18147a9c6cd7e53af
DaniloCerovic87 <chessteacher87@gmail.com>
2025-03-06 20:56:53 +0100

    Putting capital letter for Main and Child output

--- src/part1_ways_of_defining_and_basic_operations/implementing_runnable/MyRunnable.java
+++ src/part1_ways_of_defining_and_basic_operations/implementing_runnable/MyRunnable.java
@@ -5,7 +5,7 @@ public class MyRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            System.out.println("child Thread");
+            System.out.println("Child Thread");
         }
     }
 
