99041675a7cd3cff2fcb4604dbbd7bdb16af98db
DaniloCerovic87 <chessteacher87@gmail.com>
2025-03-07 10:54:30 +0100

    Thread Group creation

---
+++ src/part10_thread_groups/Test1.java
@@ -0,0 +1,14 @@
+package part10_thread_groups;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        ThreadGroup g1 = new ThreadGroup("First Group");
+        System.out.println(g1.getParent().getName());
+        ThreadGroup g2 = new ThreadGroup(g1, "Second Group");
+        System.out.println(g2.getParent().getName());
+    }
+
+
+
+}
