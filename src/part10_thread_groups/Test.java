91f7850eda8e8ddd0c1fd37370bd0dda9011f728
DaniloCerovic87 <chessteacher87@gmail.com>
2025-03-07 08:56:51 +0100

    Thread Groups main and system

---
+++ src/part10_thread_groups/Test.java
@@ -0,0 +1,10 @@
+package part10_thread_groups;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getThreadGroup().getName());
+        System.out.println(Thread.currentThread().getThreadGroup().getParent().getName());
+    }
+
+}
