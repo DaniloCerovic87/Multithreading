e1cd4da0f1bdcca262b617d467eb6833a9ab45fe
DaniloCerovic87 <chessteacher87@gmail.com>
2025-03-07 22:19:02 +0100

    ReentrantLock example

---
+++ src/part11_lock_interface/ReentrantLockDemo1.java
@@ -0,0 +1,22 @@
+package part11_lock_interface;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockDemo1 {
+
+    public static void main(String[] args) {
+        ReentrantLock l = new ReentrantLock();
+        l.lock();
+        l.lock();
+        System.out.println(l.isLocked());//true
+        System.out.println(l.isHeldByCurrentThread());//true
+        System.out.println(l.getQueueLength());//0
+        l.unlock();
+        System.out.println(l.getHoldCount());//1
+        System.out.println(l.isLocked());//true
+        l.unlock();
+        System.out.println(l.isLocked());//false
+        System.out.println(l.isFair());//false
+    }
+
+}
